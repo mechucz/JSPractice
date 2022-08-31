7bada006698361cb2984f724bb43224ba3400ed1
mechucz <mechucz@gmail.com>
2022-08-31 16:20:53 -0400

    Desafío clase 3

---
+++ assets/js/DesafioClase3.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+//const respuesta = "gato";
+
+//while(true) if(prompt("que tiene cuatro patas y maulla?: ") === respuesta) break;
+
+let respuesta = prompt("Tengo agujas y no se coser, tengo números y no sé leer");
+
+while(respuesta !== "reloj") {
+  respuesta = prompt("No lo adivinaste! intentalo de nuevo");
+}
+
+alert("Correcto!");
